a4db4187c893e9f52b42431dda1deea4531c0255
RyanSettles <ryan.settles@student.swic.edu>
2020-10-09 10:11:37 -0500

    Final Edits

--- scripts.js
+++ scripts.js
@@ -1,8 +1,5 @@
 const form =document.querySelector('form');
-const uname =document.querySelector("#username");
-const winQuote =document.querySelector("#winQuote");
-const loseQuote =document.querySelector("#loseQuote");
-
+const p =document.querySelector("p");
 
 form.addEventListener('submit', function () {
 event.preventDefault();
@@ -12,8 +9,8 @@ console.log("Your username is: " + event.target.elements[0].value + ". Your Win 
 const formData = {}
 for (let i = 0; i <= 2; i++){
   console.log(event.target.elements[i].value)
+
+  p.textContent =  ("Your username is: " + event.target.elements[0].value + ". Your Win Quote is:  " + event.target.elements[1].value + ". Your lose quote is: " + event.target.elements[2].value + ".");
 }
 });
 
-// {username: event.target.elements[0].value
-// password: event.target.elements[1].value}
